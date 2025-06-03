2b40a771f06aa1b2e5d32284062633a84da5baf4
Pengcheng <simon.peng@yml.com.cn>
2025-06-03 17:28:04 +0800

    add checkbox

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,10 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <a-checkbox v-model:checked="checked">Checkbox</a-checkbox>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const checked = ref(false);
+</script>
